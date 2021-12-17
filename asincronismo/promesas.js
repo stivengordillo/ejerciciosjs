a0f13bc753d0afd498e459763a6f399ce4b808b6
stivengordillo <stiven@stivengordillo.com>
2021-12-17 13:27:12 -0500

    promesas

---
+++ asincronismo/promesas.js
@@ -0,0 +1,27 @@
+const algoVaApasar = (value) =>{
+    return new Promise((resolve, reject) => {
+        if(true){
+            resolve('Todo bien');
+        }else{
+            reject('No funciono');
+        }
+    });
+}
+
+const pierde = (value) => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            resolve('mi pez');
+        }else{
+            reject('cara de vez');
+        }
+    });
+}
+
+Promise.all([algoVaApasar(), pierde()])
+    .then(response=> {console.log(response)})
+    .catch(err => console.log(err));
+
+//algoVaApasar()
+    //.then(response => console.log(response))
+    //.catch(err => console.error(err));
